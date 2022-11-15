bcdf440fcf9283a5bf090ebca689e76377e348e2
Aden Kenny <adenkenny@gmail.com>
2022-11-15 22:29:10 +1300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ stage.addChild(hudContainer);
 stage.addChild(devContainer);
 
 var ticker = new PIXI.Ticker();
-var loader = new PIXI.Loader.shared;
+var loader = new PIXI.Loader();
 
 var resourceManager = new ResourceManager(loader);
 
